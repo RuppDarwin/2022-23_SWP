bfeb91890b27fa09e499be18a3e13be7d0ee436b
RuppDarwin <ruppdarwin@gmail.com>
2022-10-13 11:27:49 +0200

    new version

--- 02_JS/11_Aufgabe/03_JS/11_Aufgabe_script.js
+++ 02_JS/11_Aufgabe/03_JS/11_Aufgabe_script.js
@@ -5,5 +5,23 @@ document.getElementById("addTask").addEventListener("click", function(){addTask(
 function addTask(){
     let taskname = document.getElementById("newtaskname").value;
     let taskrespons = document.getElementById("newtaskrespons").value;
-    let task = { name: taskname, }
+    let newtask = { name: taskname, respons: taskrespons, isDone:false};
+    tasks.push(newtask);
+    printTaskList();
+}
+
+function printTaskList(){
+    document.getElementById("tasklist").innerHTML = getHTMLTasks();
+}
+
+function getHTMLTasks(){
+    let html = "";
+    tasks.forEach(Element => {
+        let checked = "";
+        if(Element.isDone == true){
+            checked = "checked"
+        }
+        html += "<li><input type='checkbox' " + checked + "/>" + Element.name + "-" + Element.respons;
+    });
+    return html;
 }
